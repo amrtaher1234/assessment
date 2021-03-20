46d65eec4c5ab52c4d6a79aa8b8ab0ae2a0ef8fd
Your Name <sekab>
2021-03-21 01:54:02 +0200

    adding invalid test cases

--- js-numerals/tests/js-numerals.test.js
+++ js-numerals/tests/js-numerals.test.js
@@ -1,12 +1,13 @@
 const numberToWords = require('../solution');
-const {oneDigitsTests, twoDigitsTests, nDigitsTests} = require('./js-numerals.test.json');
+const {invalidTests, nDigitsTests} = require('./js-numerals.test.json');
 
-test.each(oneDigitsTests)("test with one digits" , (data) => {
+
+test.each(nDigitsTests)("test with N digits" , (data) => {
   const {number, expectedResult} = data;
   expect(numberToWords(number)).toBe(expectedResult);
 })
 
-test.each(nDigitsTests)("test with N digits" , (data) => {
+test.each(invalidTests)("test with invalid cases (empty, two negatives, above 72 digits." , (data) => {
   const {number, expectedResult} = data;
   expect(numberToWords(number)).toBe(expectedResult);
 })
