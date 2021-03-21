b5131ec03d25d5330c29ce60b6816506c648fe3f
Your Name <sekab>
2021-03-21 14:12:52 +0200

    adding remove leading zeroes function

--- js-numerals/helpers.js
+++ js-numerals/helpers.js
@@ -1,14 +1,20 @@
 function removeTrailingZeros(num = "") {
     for (let i = num.length - 1; i>=0; i--) {
         if (num.charAt(i) !== "0") {
-            return num.substr(0, i);
+            return num.substr(0, i+1);
         }
     }
     return num;
 }
+function removeLeadingZeros(num = "") {
+    return num.replace(/^0+/, "");
+}
+const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
 
 var helpers = {
-    removeTrailingZeros
+    removeTrailingZeros,
+    removeLeadingZeros,
+    pipe
 }
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = helpers
